1b40dbf93ea8bd32464b615d5b0e39f9eb1cf607
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-19 13:33:48 +0000

    Updating index.html with initial grid layout and adding timer to script

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ var reformatDate = dayjs(today, "DD-MM-YY").format(
   "dddd, D MMMM YYYY, h:mm:ss a"
 );
 $("#currentDay").text(reformatDate);
+
+console.log("Creating Planner area: ");
+
+var store = window.localStorage;
+var container = $(".container");
+var now = dayjs();
+
+var currentTime = { text: dayjs().format("h:00 A"), hour: dayjs().hour() };
+//---------------------------------------------------------------------------------
+console.log("current Time is: ");
+console.log(currentTime);
+
+function color(time) {
+  return time.text === currentTime.text
+    ? "bg-red-300"
+    : time.hour < now
+    ? "bg-gray-300"
+    : "bg-green-200";
+}
