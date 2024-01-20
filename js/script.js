66e52173adbc9e3439d53da5b8dbc3a79d88b331
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-20 21:49:43 +0000

    Initial schema to render persisted tasks values from localStorage.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ $("#currentDay").text(reformatDate);
 var currentHour = dayjs().format("HH");
 
 console.log("Creating Planner area: ");
+// set variables for preiously persisted values for each task
+var textTaskSpan = document.querySelector(".description");
+var hourTaskSpan = document.querySelector("id");
 
 //var store = window.localStorage;
 //var container = $(".container");
@@ -30,6 +33,34 @@ console.log("Value of Container Variable: ");
 // console.log(displayEl);
 // console.log(buttonListEl);
 
+//--- load previous saved tasks
+function renderLastRegisteredTask() {
+  //
+  //
+  var textValue = localStorage.getItem("11"); // must try to use the correct KEY
+  var textValue = localStorage.getItem("17"); // must try to use the correct KEY
+  var textValue = localStorage.getItem("18"); // must try to use the correct KEY
+
+  //var hourValue = localStorage.getItem(Key);
+
+  console.log("Value of Saved tasks in localStorage");
+  console.log(textValue);
+  //console.log(hourValue);
+
+  textTaskSpan.textContent = textValue;
+
+  //$("#hour-17 .time-block").val(localStorage.getItem("17"));
+  $("#hour-11 .description").val(textTaskSpan);
+  $("#hour-17 .description").val(textTaskSpan);
+  $("#hour-18 .description").val(textTaskSpan);
+
+  // hourTaskSpan.textContent = hourValue;
+
+  // console.log("value of lastEmail");
+  // console.log(userEmailSpan);
+}
+renderLastRegisteredTask();
+
 //----------------------------------- Sample variables for each of the Description Text value
 
 var hour08Input = document.querySelector("#hour-08");
@@ -37,6 +68,17 @@ var hour09Input = document.querySelector("#hour-09");
 var hour10Input = document.querySelector("#hour-10");
 var hour11Input = document.querySelector("#hour-11");
 var hour12Input = document.querySelector("#hour-12");
+var hour13Input = document.querySelector("#hour-13");
+var hour14Input = document.querySelector("#hour-14");
+var hour15Input = document.querySelector("#hour-15");
+var hour16Input = document.querySelector("#hour-16");
+var hour17Input = document.querySelector("#hour-17");
+var hour18Input = document.querySelector("#hour-18");
+var hour19Input = document.querySelector("#hour-19");
+var hour20Input = document.querySelector("#hour-20");
+
+console.log("Value of hou17Inout: ");
+console.log(hour17Input.value);
 //
 
 // need to capture de text fields and save the positions into localstorage to be recalled later
@@ -55,7 +97,10 @@ $(".saveBtn").click(function (event) {
   }
 
   //=---------------------------
-  else console.log("Error captured: ");
+  else
+    console.log(
+      "Error captured: Need to enter value for a task before saving time block! "
+    );
 });
 // need to include validation so that you cannot save row when value of description is null
 //-------------
@@ -82,3 +127,8 @@ $(".description").each(function () {
     $(this).addClass("past");
   }
 });
+
+// Tasks remaining:
+// --------  load tasks saved in localStorage
+
+//---- if time include a clear button eliminate task from time block and local Storage (Nice to have functionality)
