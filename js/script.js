8fb65b08a97f65d58e793c27fc5b7ff393a3ec40
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-20 17:21:14 +0000

    Adding logic to save each line created.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // putting initial current date
 // initial timer for daily Planner
+var displayEl = $("#hour");
+var buttonListEl = $("#buttons");
 console.log("Consoloe Log enabled");
 var today = dayjs();
 var reformatDate = dayjs(today, "DD-MM-YY").format(
@@ -11,17 +13,45 @@ console.log("Creating Planner area: ");
 
 var store = window.localStorage;
 var container = $(".container");
-var now = dayjs();
+
+//var now = dayjs();
 
 var currentTime = { text: dayjs().format("h:00 A"), hour: dayjs().hour() };
 //---------------------------------------------------------------------------------
 console.log("current Time is: ");
 console.log(currentTime);
 
-function color(time) {
-  return time.text === currentTime.text
-    ? "bg-red-300"
-    : time.hour < now
-    ? "bg-gray-300"
-    : "bg-green-200";
-}
+console.log("Value of Container Variable: ");
+console.log(container);
+
+//console.log(container.attribu);
+// console.log(container.childnodes);
+console.log("value displayEl: ");
+console.log(displayEl);
+console.log(buttonListEl);
+
+//----------------------------------- Variables for each of the Text
+
+var hour08Input = document.querySelector("#hour-08");
+var hour09Input = document.querySelector("#hour-09");
+var hour10Input = document.querySelector("#hour-10");
+var hour11Input = document.querySelector("#hour-11");
+var hour12Input = document.querySelector("#hour-12");
+//
+var saveButtonEl = document.querySelector(".saveButton");
+saveButtonEl.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  //   //
+  //   // create user object from submission
+  //var calendarHour =
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id").split("-")[01];
+  // captures the second array of hour numerical to save
+  localStorage.setItem(time, value);
+
+  // set new submission
+  // localStorage.setItem("calendarHour", JSON.stringify(calendarHour));
+  //console.log("Value of calendarHour: ");
+  //console.log(calendarHour);
+});
