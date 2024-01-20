b73df8879b529b522169e04bd577a0ae3afb26d2
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-20 19:55:55 +0000

    Added function to compare current Hour to paint Description Text either grey, red or green

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 // putting initial current date
 // initial timer for daily Planner
-var displayEl = $("#hour");
-var buttonListEl = $("#buttons");
+// var displayEl = $("#hour");
+// var buttonListEl = $("#buttons");
 console.log("Consoloe Log enabled");
 var today = dayjs();
 var reformatDate = dayjs(today, "DD-MM-YY").format(
   "dddd, D MMMM YYYY, h:mm:ss a"
 );
 $("#currentDay").text(reformatDate);
+var currentHour = dayjs().format("HH");
 
 console.log("Creating Planner area: ");
 
-var store = window.localStorage;
-var container = $(".container");
+//var store = window.localStorage;
+//var container = $(".container");
 
 //var now = dayjs();
 
@@ -22,13 +23,12 @@ console.log("current Time is: ");
 console.log(currentTime);
 
 console.log("Value of Container Variable: ");
-console.log(container);
+//console.log(container);
 
-//console.log(container.attribu);
-// console.log(container.childnodes);
-console.log("value displayEl: ");
-console.log(displayEl);
-console.log(buttonListEl);
+//console.log(container.children);
+// console.log("value displayEl: ");
+// console.log(displayEl);
+// console.log(buttonListEl);
 
 //----------------------------------- Variables for each of the Text
 
@@ -38,20 +38,38 @@ var hour10Input = document.querySelector("#hour-10");
 var hour11Input = document.querySelector("#hour-11");
 var hour12Input = document.querySelector("#hour-12");
 //
-var saveButtonEl = document.querySelector(".saveButton");
-saveButtonEl.addEventListener("click", function (event) {
+
+// need to capture de text fields and save the positions into localstorage to be recalled later
+
+//Saves to localStorage text element and time element for each task entered
+$(".saveBtn").click(function (event) {
   event.preventDefault();
-
-  //   //
-  //   // create user object from submission
-  //var calendarHour =
-  var value = $(this).siblings(".description").val();
-  var time = $(this).parent().attr("id").split("-")[01];
-  // captures the second array of hour numerical to save
+  var value = $(this).siblings(".description").val(); // captures the text element for the task
+  var time = $(this).parent().attr("id").split("-")[1]; // captures the hour number for the row parent id
   localStorage.setItem(time, value);
+});
+// need to include validation so that you cannot save row when value of description is null
+//-------------
 
-  // set new submission
-  // localStorage.setItem("calendarHour", JSON.stringify(calendarHour));
-  //console.log("Value of calendarHour: ");
-  //console.log(calendarHour);
+//---------
+// Defines function for Description Class to compara hour (timeDiv) from each time slot vs CurrentHour variable
+//validates time slot and compares it to current time to colour according to past (grey), present (red) future(green)
+//
+$(".description").each(function () {
+  var timeDiv = $(this).parent().attr("id").split("-")[1];
+  console.log("value of timeDiv: ");
+  console.log(timeDiv);
+  console.log("value of currentHour: ");
+  console.log(currentHour);
+
+  if (currentHour === timeDiv) {
+    $(this).addClass("present");
+    $(this).children(".description").addClass("white-text");
+  } else if (currentHour < timeDiv) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  } else if (currentHour > timeDiv) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
 });
