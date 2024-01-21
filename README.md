077b03b86651e62d8b4224fecc6000cbb5871eeb
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-21 16:35:08 +0000

    Addedd final validation Alert and modified persistance capture from localStorage.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# dailyplanner
-Challenge 07 - Planner App
+# Third-Party APIs: Work Day Scheduler
+
+Challenge 07 corresponds to the creation of a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+The application will use DayJS library to format date and time attributes.
+
+Main Application - Screen Capture 1
+
+## Acceptance Criteria
+
+The app should:
+
+- Display the current day at the top of the calender when a user opens the planner.
+
+- Present timeblocks for standard business hours when the user scrolls down.
+
+- Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+- Allow a user to enter an event when they click a timeblock
+
+- Save the event in local storage when the save button is clicked in that timeblock.
+
+- Persist events between refreshes of a page
+
+The following Screen captures demonstrates the application functionality operational:
+
+## Screen Capture 1: Main Application operational with Console Log output and localStorage of timeblock text
+
+(./images/ScreenCapture1.png)
+
+![Screen Capture1](./images/ScreenCapture1.png)
+
+## Screen Capture 2: Main Application showing Error capture so that no persistance of null timeblocks
+
+![Screen Capture2](./images/ScreenCapture2.png)
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // initial timer for daily Planner
 // var displayEl = $("#hour");
 // var buttonListEl = $("#buttons");
-console.log("Consoloe Log enabled");
+console.log("Console Log enabled");
 var today = dayjs();
 var reformatDate = dayjs(today, "DD-MM-YY").format(
   "dddd, D MMMM YYYY, h:mm:ss a"
@@ -12,8 +12,8 @@ var currentHour = dayjs().format("HH");
 
 console.log("Creating Planner area: ");
 // set variables for preiously persisted values for each task
-var textTaskSpan = document.querySelector(".description");
-var hourTaskSpan = document.querySelector("id");
+// var textTaskSpan = document.querySelector(".description");
+// var hourTaskSpan = document.querySelector("id");
 
 //var store = window.localStorage;
 //var container = $(".container");
@@ -37,9 +37,22 @@ console.log("Value of Container Variable: ");
 function renderLastRegisteredTask() {
   //
   //
-  var textValue11 = localStorage.getItem("11", JSON.parse(11)); // must try to use the correct KEY
-  var textValue17 = localStorage.getItem("17", JSON.parse(17)); // must try to use the correct KEY
-  var textValue18 = localStorage.getItem("18", JSON.parse(18)); // must try to use the correct KEY
+
+  // Identify the Key to be loaded (individual hour) and pass to variable textValue. Try to use correct KEY
+  var textValue08 = localStorage.getItem("08", JSON.parse(8));
+  var textValue09 = localStorage.getItem("09", JSON.parse(9));
+  var textValue10 = localStorage.getItem("10", JSON.parse(10));
+  var textValue11 = localStorage.getItem("11", JSON.parse(11));
+  var textValue12 = localStorage.getItem("12", JSON.parse(12));
+  var textValue13 = localStorage.getItem("13", JSON.parse(13));
+  var textValue14 = localStorage.getItem("14", JSON.parse(14));
+  var textValue15 = localStorage.getItem("15", JSON.parse(15));
+  var textValue16 = localStorage.getItem("16", JSON.parse(16));
+  var textValue17 = localStorage.getItem("17", JSON.parse(17));
+  var textValue18 = localStorage.getItem("18", JSON.parse(18));
+  var textValue19 = localStorage.getItem("19", JSON.parse(19));
+  var textValue20 = localStorage.getItem("20", JSON.parse(20));
+  var textValue21 = localStorage.getItem("21", JSON.parse(21));
 
   //var hourValue = localStorage.getItem(Key);
 
@@ -49,10 +62,22 @@ function renderLastRegisteredTask() {
 
   //textTaskSpan.textContent = textValue;
 
-  //$("#hour-17 .time-block").val(localStorage.getItem("17"));
+  //-----
+  // Pass
+  $("#hour-08 .description").val(textValue08);
+  $("#hour-09 .description").val(textValue09);
+  $("#hour-10 .description").val(textValue10);
   $("#hour-11 .description").val(textValue11);
+  $("#hour-12 .description").val(textValue12);
+  $("#hour-13 .description").val(textValue13);
+  $("#hour-14 .description").val(textValue14);
+  $("#hour-15 .description").val(textValue15);
+  $("#hour-16 .description").val(textValue16);
   $("#hour-17 .description").val(textValue17);
   $("#hour-18 .description").val(textValue18);
+  $("#hour-19 .description").val(textValue19);
+  $("#hour-20 .description").val(textValue20);
+  $("#hour-21 .description").val(textValue21);
 
   // var lastUser = localStorage.getItem("user", JSON.parse(user));
 
@@ -65,23 +90,23 @@ renderLastRegisteredTask();
 
 //----------------------------------- Sample variables for each of the Description Text value
 
-var hour08Input = document.querySelector("#hour-08");
-var hour09Input = document.querySelector("#hour-09");
-var hour10Input = document.querySelector("#hour-10");
-var hour11Input = document.querySelector("#hour-11");
-var hour12Input = document.querySelector("#hour-12");
-var hour13Input = document.querySelector("#hour-13");
-var hour14Input = document.querySelector("#hour-14");
-var hour15Input = document.querySelector("#hour-15");
-var hour16Input = document.querySelector("#hour-16");
-var hour17Input = document.querySelector("#hour-17");
-var hour18Input = document.querySelector("#hour-18");
-var hour19Input = document.querySelector("#hour-19");
-var hour20Input = document.querySelector("#hour-20");
+// var hour08Input = document.querySelector("#hour-08");
+// var hour09Input = document.querySelector("#hour-09");
+// var hour10Input = document.querySelector("#hour-10");
+// var hour11Input = document.querySelector("#hour-11");
+// var hour12Input = document.querySelector("#hour-12");
+// var hour13Input = document.querySelector("#hour-13");
+// var hour14Input = document.querySelector("#hour-14");
+// var hour15Input = document.querySelector("#hour-15");
+// var hour16Input = document.querySelector("#hour-16");
+// var hour17Input = document.querySelector("#hour-17");
+// var hour18Input = document.querySelector("#hour-18");
+// var hour19Input = document.querySelector("#hour-19");
+// var hour20Input = document.querySelector("#hour-20");
 
-console.log("Value of hou17Inout: ");
-console.log(hour17Input.value);
-//
+// console.log("Value of hou17Inout: ");
+// console.log(hour17Input.value);
+// //
 
 // need to capture de text fields and save the positions into localstorage to be recalled later
 
@@ -95,36 +120,36 @@ $(".saveBtn").click(function (event) {
   if (value != "") {
     // save to localStorage
     localStorage.setItem(time, value);
-    console.log("success", "Registered successfully");
+    console.log("success", "Task registered successfully");
   }
 
   //=---------------------------
-  else
-    console.log(
-      "Error captured: Need to enter value for a task before saving time block! "
-    );
+  else alert("Error: Please enter a value for a task before saving!");
+  // console.log(
+  //   "Error captured: Need to enter value for a task before saving time block! "
+  // );
 });
 // need to include validation so that you cannot save row when value of description is null
 //-------------
 
 //---------
-// Defines function for Description Class to compara hour (timeDiv) from each time slot vs CurrentHour variable
+// Defines function for Description Class to compare hour (timeBlock) from each time slot vs CurrentHour variable
 //validates time slot and compares it to current time to colour according to past (grey), present (red) future(green)
 //
 console.log("value of currentHour: ");
 console.log(currentHour);
 $(".description").each(function () {
-  var timeDiv = $(this).parent().attr("id").split("-")[1];
-  console.log("value of timeDiv: ");
-  console.log(timeDiv);
+  var timeBlock = $(this).parent().attr("id").split("-")[1];
+  // console.log("value of timeBlock: ");
+  //console.log(timeBlock);
 
-  if (currentHour === timeDiv) {
+  if (currentHour === timeBlock) {
     $(this).addClass("present");
     $(this).children(".description").addClass("white-text");
-  } else if (currentHour < timeDiv) {
+  } else if (currentHour < timeBlock) {
     $(this).removeClass("present");
     $(this).addClass("future");
-  } else if (currentHour > timeDiv) {
+  } else if (currentHour > timeBlock) {
     $(this).removeClass("future");
     $(this).addClass("past");
   }
